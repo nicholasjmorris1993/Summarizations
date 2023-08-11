a5927c7b525c72813bd5e0b164b3bbb5b003c74b
Nick <129078463+nicholasjmorris1993@users.noreply.github.com>
2023-08-11 17:00:13 -0400

    Add files via upload

--- test/test_summary.py
+++ test/test_summary.py
@@ -1,1 +1,11 @@
+import pandas as pd
+import sys
+sys.path.append("/home/nick/Summarizations/src")
+from summary import describe
 
+
+data = pd.read_csv("/home/nick/Summarizations/test/LungCap.csv")
+
+metrics = describe(data)
+
+metrics.stats
